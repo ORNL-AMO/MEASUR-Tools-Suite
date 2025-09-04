a2484234db753337152915317d2d892c2dfaf5b1
Mark Root <rootrm@ornl.gov>
2025-09-04 16:12:29 -0500

    revert changes to catch.hpp

--- tests/cpp/catch_isDebuggerActive_stub.cpp
+++
@@ -1,5 +0,0 @@
-// Stub for Catch::isDebuggerActive for Apple Silicon/arm64
-// Place in test build to resolve linker error with old Catch2
-namespace Catch {
-bool isDebuggerActive() { return false; }
-}
