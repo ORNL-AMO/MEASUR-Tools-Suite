883b21ce977e2a5203b855652ca9d5c51e21ac7f
Liam White <whiteln@ornl.gov>
2025-09-09 15:55:16 -0400

    docs: update documentation references for atmosphere heat loss calculator

--- include/processHeat/losses/atmosphere_heat_loss.h
+++ include/processHeat/losses/atmosphere_heat_loss.h
@@ -23,7 +23,7 @@ namespace atmosphere_heat_loss {
  * @struct GasType
  * @brief Represents a gas type and its associated specific heat at a reference temperature of
  * @qty{60;\degreeFahrenheit}.
- * @see specific_heat_values
+ * @see atmosphere_specific_heat_values
  */
 struct GasType {
     std::string gas_description; ///< Description of the gas type.
@@ -34,7 +34,7 @@ struct GasType {
  * @ingroup atmosphere_heat_loss_calculator
  * @brief A collection of specific heat values for common atmospheric gases at a reference temperature of
  * @qty{60;\degreeFahrenheit}.
- * @see specific_heat_values
+ * @see atmosphere_specific_heat_values
  */
 inline const std::array<GasType, 6> kGasTypes {{
     {"Nitrogen", 0.0185},
@@ -50,7 +50,7 @@ inline const std::array<GasType, 6> kGasTypes {{
  * @brief Returns the predefined specific heat values for common atmospheric gases at a reference temperature of
  * @qty{60;\degreeFahrenheit}.
  * @return A vector of GasType structs containing gas descriptions and their associated specific heat values.
- * @see specific_heat_values
+ * @see atmosphere_specific_heat_values
  */
 inline const std::vector<GasType>& gasTypes() {
     static const std::vector<GasType> gas_types_vector(kGasTypes.begin(), kGasTypes.end());
@@ -67,8 +67,8 @@ inline const std::vector<GasType>& gasTypes() {
  * @param[in] correction_factor Correction factor @unitb{\unitless}
  * @return Total heat loss @unitb{\btu\per\hour}
  * @see
- * - atmosphere_total_heat_loss_formula
- * - specific_heat_values
+ * - atmosphere_heat_loss_formula
+ * - atmosphere_specific_heat_values
  */
 double totalHeatLoss(double flow_rate, double specific_heat, double inlet_temperature, double outlet_temperature,
                      double correction_factor);
