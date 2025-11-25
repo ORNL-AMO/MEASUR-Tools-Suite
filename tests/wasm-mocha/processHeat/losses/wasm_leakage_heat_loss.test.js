66a5913369feff99771e51f49c1446dc4d4da652
Mark Root <rootrm@ornl.gov>
2025-11-25 11:55:06 -0600

    fix mocha test

--- tests/wasm-mocha/processHeat/losses/wasm_leakage_heat_loss.test.js
+++ tests/wasm-mocha/processHeat/losses/wasm_leakage_heat_loss.test.js
@@ -13,7 +13,7 @@ describe('Process Heat Leakage Heat Loss', function () {
             draftPressure: 0.1, openingArea: 3, leakageGasTemperature: 1600, ambientTemperature: 80,
             coefficientDischarge: 0.8052, specificGravity: 1.02, correctionFactor: 1.0
         };
-        var leakageTotalHeatLoss = moduleInstance.leakageTotalHeatLoss(inp.draftPressure, inp.openingArea, inp.leakageGasTemperature, inp.ambientTemperature, inp.coefficient, inp.specificGravity, inp.correctionFactor);
+        var leakageTotalHeatLoss = moduleInstance.leakageTotalHeatLoss(inp.draftPressure, inp.openingArea, inp.leakageGasTemperature, inp.ambientTemperature, inp.coefficientDischarge, inp.specificGravity, inp.correctionFactor);
         assert.equal(leakageTotalHeatLoss, 2850767.216228273);
     });
 });
