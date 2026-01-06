0e421eec7c38beaed0dac5c3a3103cff69017a2c
Mark Root <rootrm@ornl.gov>
2026-01-06 13:13:10 -0600

    fixup: rename variable for accuracy

--- src/processHeat/losses/auxiliary_power_used.cpp
+++ src/processHeat/losses/auxiliary_power_used.cpp
@@ -7,12 +7,12 @@
 
 namespace auxiliary_power_used {
 
-double calculatePowerUsed(double motor_phase, double supply_voltage, double avg_current, double power_factor,
+double calculatePowerUsed(double number_of_motor_phases, double supply_voltage, double avg_current, double power_factor,
                     double operating_time) {
               // Convert percent inputs to fractions
               const double ot_frac = operating_time / 100.0;
               // Electrical power in kW
-              const double electrical_kw = std::pow(motor_phase, 0.5) * supply_voltage * avg_current * power_factor * ot_frac / 1000.0;
+              const double electrical_kw = std::pow(number_of_motor_phases, 0.5) * supply_voltage * avg_current * power_factor * ot_frac / 1000.0;
               // Convert kW to BTU/hr using constant
               const double btu_per_hr = electrical_kw * physics::conversions::kKilowattToBtuPerHour;
               return btu_per_hr;
